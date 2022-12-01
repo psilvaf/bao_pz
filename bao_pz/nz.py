727b439cd6c63f760b8039911ca2cb22423f9e94
Paula Ferreira <51297334+psilvaf@users.noreply.github.com>
2022-12-01 16:08:36 -0300

    round

--- bao_pz/nz.py
+++ bao_pz/nz.py
@@ -5,6 +5,10 @@ import os
 import time
 from multiprocessing import Pool
 
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
 def distz(z_dist,binsize,survey_bins,output_folder):
 	
 	'''Computes the average number density of the spec-z distribution (not normalised)
@@ -20,8 +24,8 @@ def distz(z_dist,binsize,survey_bins,output_folder):
 	for l in range(len(survey_bins)):
 		for i in survey_bins[l]['Z']:
 			for j in z_dist:
-				if i==j:
-					COUNT[l].append(i)					
+				if truncate(i,2)==j:
+					COUNT[l].append(truncate(i,2))					
 				
 	for r in range(len(COUNT)):
 		np.save(output_folder+str(r),np.histogram(COUNT[r],bins=binsize))
