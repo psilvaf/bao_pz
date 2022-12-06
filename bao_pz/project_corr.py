14c7bb46e434957cbea23fc0c8510b6286b1cd28
Paula Ferreira <51297334+psilvaf@users.noreply.github.com>
2022-12-06 11:11:59 -0300

    6dez

--- bao_pz/project_corr.py
+++ bao_pz/project_corr.py
@@ -6,19 +6,22 @@ from Corrfunc.utils import convert_rp_pi_counts_to_wp
 from Corrfunc.mocks.DDrppi_mocks import DDrppi_mocks
 from astropy.cosmology import FlatLambdaCDM
 
-def p_corr(survey,random,binfile,output_file,cosmology=FlatLambdaCDM(H0=70, Om0=0.25, Tcmb0=2.725,Ob0=0.044)):
+def p_corr(survey,random,output_file,nbins=40, pimax=175,cosmology=FlatLambdaCDM(H0=70, Om0=0.25, Tcmb0=2.725,Ob0=0.044)):
 	# Setup the bins
 	print(cosmology)
 	dist=cosmology.comoving_distance(survey['Z'])
 	dist2=cosmology.comoving_distance(random['Z'])
 	nthreads = 4
-	pimax=120
-	DD_counts = DDrppi_mocks(autocorr=1, cosmology=1, nthreads=nthreads,pimax=pimax, binfile=binfile,RA1=survey['RA'],DEC1= survey['DEC'],CZ1=dist, weights1=survey['WTOT'],weight_type='pair_product',is_comoving_dist=True)
+	bins = np.linspace(40, 140.0, nbins + 1)
+	pimax=pimax
+	autocorr=1
+	cosmology=1
+	DD_counts = DDrppi_mocks(autocorr, cosmology, nthreads,pimax, bins,survey['RA'],survey['DEC'],dist, survey['WTOT'],weight_type='pair_product',is_comoving_dist=True)
 	
-	RR_counts = DDrppi_mocks(autocorr=1, cosmology=1,  nthreads=nthreads,pimax=pimax, binfile=binfile,RA1=random['RA'], DEC1=random['DEC'],CZ1=dist2, weights1=random['WEIGHT'],weight_type='pair_product',is_comoving_dist=True)
+	RR_counts = DDrppi_mocks(autocorr, cosmology,  nthreads,pimax, bins,random['RA'], random['DEC'],dist2, random['WEIGHT'],weight_type='pair_product',is_comoving_dist=True)
 	
-	DR_counts = DDrppi_mocks(autocorr=1, cosmology=1, nthreads=nthreads,pimax=pimax, binfile=binfile,RA1=survey['RA'], DEC1=survey['DEC'],CZ1=dist,weights1=survey['WTOT'],RA2=random['RA'], DEC2=random['DEC'],CZ2=dist2, weights2=random['WEIGHT'],weight_type='pair_product',is_comoving_dist=True)
+	DR_counts = DDrppi_mocks(autocorr, cosmology, nthreads,pimax, bins,survey['RA'], survey['DEC'],dist,survey['WTOT'],random['RA'], random['DEC'],dist2, random['WEIGHT'],weight_type='pair_product',is_comoving_dist=True)
 	
-	xi=convert_rp_pi_counts_to_wp(len(survey), len(survey), len(random), len(random),DD_counts['npairs'], DR_counts['npairs'],DR_counts['npairs'], RR_counts['npairs'])
+	xi=convert_rp_pi_counts_to_wp(len(survey), len(survey), len(random), len(random),DD_counts['npairs'], DR_counts['npairs'],DR_counts['npairs'], RR_counts['npairs'],nbins, pimax)
 	return  np.save(output_file,xi)
 
