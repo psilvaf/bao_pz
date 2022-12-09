ee7ae5d0996dd3ebc76c359e3c0dfd5762f16946
Paula Ferreira <psilf12@gmail.com>
2022-12-09 08:38:53 -0300

    pknw

--- build/lib/bao_pz/nz.py
+++ build/lib/bao_pz/nz.py
@@ -38,10 +38,23 @@ def distzp(binsize,survey_bins,output_folder):
 	survey_bin(fits Table): data
 	output_folder(str): file path
 	
+<<<<<<< Updated upstream
 	return the number counts of the photometric redshift of a z_dist
 	'''	
 	COUNT=[np.histogram(survey_bins[l]['Z'], bins=binsize) for l in range(len(survey_bins))]
 	
+=======
+def distzp(binsize,survey_bins,output_folder):
+	'''Computes the average number density of the spec-z distribution (not normalised)
+	binsize(float): size of photo-z PDF/defined bins
+	survey_bin(fits Table): data
+	output_folder(str): file path
+	
+	return the number counts of the photometric redshift of a z_dist
+	'''	
+	COUNT=[np.histogram(survey_bins[l]['Z'], bins=binsize) for l in range(len(survey_bins))]
+	
+>>>>>>> Stashed changes
 	for r in range(len(COUNT)):
 		np.save(output_folder+str(r),COUNT[r])
 	return
