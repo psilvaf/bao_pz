e222088f7878dadabae86d6d675715c8bb93eff5
Paula Ferreira <psilf12@gmail.com>
2023-06-07 15:36:03 -0300

    jun7

--- bao_pz/Resample.py
+++ bao_pz/Resample.py
@@ -9,10 +9,6 @@ import time
 from scipy.interpolate import CubicSpline
 from scipy import integrate
 
-def truncate(n, decimals=0):
-    multiplier = 10 ** decimals
-    return int(n * multiplier) / multiplier
-
 def Distz(z_dist,zname,binsize,survey,output_folder):
 	
 	'''Computes the photo-z distribution(matched to the training set) ,
@@ -26,23 +22,24 @@ def Distz(z_dist,zname,binsize,survey,output_folder):
 	return: the number counts of the redshift of a z_dist
 	'''
 	
-	COUNT=[i for i in survey[zname] for j in z_dist if truncate(i,3)==j]
+	COUNT=np.histogram(survey[zname][np.where(abs(z_dist-survey[zname])<=.01)],bins=binsize)
 				
 				
-	np.save(output_folder,np.histogram(COUNT,bins=binsize))
+	np.save(output_folder,COUNT)
 	return 
 	
-def NZ_survey(n_train_zs,g_zp,n_train_zp,n_survey,z1,z2):
-	gzp=CubicSpline(g_zp[1][:-1],g_zp[0])
-	NZp=CubicSpline(n_train_zp[1][:-1],n_train_zp[0])
-	NZ_survey_zp=CubicSpline(n_survey[1][:-1],n_survey[0])
+def NZ_survey(n_train_zs,g_zp,n_train_zp,n_survey):
+	gzp=CubicSpline(g_zp[1][:-1],g_zp[0]/np.sum(g_zp[0]))
+	NZp=CubicSpline(n_train_zp[1][:-1],n_train_zp[0]/np.sum(n_train_zp[0]))
+	NZ_survey_zp=CubicSpline(n_survey[1][:-1],n_survey[0]/np.sum(n_survey[0]))
 	func=lambda zp: n_train_zs[0]*NZ_survey_zp(zp)*gzp(zp)/NZp(zp)
 	
-	return n_train_zs[1][:-1],integrate.quad_vec(func,z1,z2)[0]
+	return n_train_zs[1][:-1],integrate.quad_vec(func,0,np.inf)[0]
 	
 def n_z(bins,zname,nz_survey):
 	count=[]
 	for i in range(len(bins)):
-		count.append([np.where(np.round(bins[i][zname],2)==k) for k in np.round(nz_survey,2)])
+		count.append([np.where(abs(bins[i][zname]-k)<=.01) for k in nz_survey])
+		
 	normalized_count=[np.array([len(i[0]) for i in j])/np.sum([len(i[0]) for i in j]) for j in count]
 	return normalized_count
--- bao_pz/get_bins.py
+++ bao_pz/get_bins.py
@@ -7,11 +7,12 @@ from multiprocessing import Pool
 from astropy.table import Table
 import time
 
-def bins(survey,z_name,num,path,zp1,zp2):
+def bins(survey,z_name,path,zp1,zp2,width):
 	start = time.time()
 	
 	data = fits.open(os.path.join(survey))[1].data
-	zbin=np.linspace(zp1,zp2,num)
+	zbin=np.arange(zp1,zp2,width)
+	print(zbin)
 	with Pool() as pool:
 		for i in range(len(zbin)-1):
 			tab=data[(data[z_name]>zbin[i]) & (data[z_name]<=zbin[i+1])]
