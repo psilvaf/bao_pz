3fae3ab618242a2f8f725271aa0fd142c2bc84c8
Paula Ferreira <psilf12@gmail.com>
2023-07-03 16:34:42 -0300

    jul3

--- bao_pz/pdfs_cut.py
+++ bao_pz/pdfs_cut.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import numpy as np
 import os
+from scipy.signal import find_peaks
 from astropy.table import Table
 import os.path as path
 from scipy import stats
@@ -26,6 +27,6 @@ def find_multi_modal(data,redshift_name,pdf_name,num_of_peaks):
 	pdf_name: pdf column name
 	num_of_peaks: number of peaks in a pdf
 	'''
-    peaks=[find_peaks(data[pdf_name][i])[0] for i in range(len(data))]
-    less_peaks=[i for i in range(len(peaks)) if len(peaks[i])==num_of_peaks]
-    return less_peaks
+	peaks=[find_peaks(data[pdf_name][i])[0] for i in range(len(data))]
+	less_peaks=[i for i in range(len(peaks)) if len(peaks[i])==num_of_peaks]	
+	return less_peaks
