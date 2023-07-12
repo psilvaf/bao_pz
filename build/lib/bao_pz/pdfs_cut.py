071341ba46b91f567f8b674bb69a10e97845f58b
Paula Ferreira <psilf12@gmail.com>
2023-07-12 14:36:24 -0300

    jul12

--- build/lib/bao_pz/pdfs_cut.py
+++ build/lib/bao_pz/pdfs_cut.py
@@ -9,13 +9,13 @@ from scipy import stats
 
 def pdfs_peaks_criteria(data,redshift_name,pdf_name,num_of_peaks):
 	'''
-	Find pdfs with more peaks that are less than 80% of the main peak
+	Find pdfs with more peaks that are less than 30% of the main peak
 	data: fits table
 	redshift_name: redshift columns name
 	pdf_name: pdf column name
 	num_of_peaks: number of peaks in a pdf
 	'''
-	peaks=[find_peaks(data[pdf_name][i],height=max(data[pdf_name][i])*.8,)[0] for i in range(len(data))]
+	peaks=[find_peaks(data[pdf_name][i],height=max(data[pdf_name][i])*.3,)[0] for i in range(len(data))]
 	less_peaks=[i for i in range(len(peaks)) if len(peaks[i])<=num_of_peaks]
 	return less_peaks
     
