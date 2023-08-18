7e17aaf6696d28fd1d3745b6a0be60f17c5829bc
Paula Ferreira <psilf12@gmail.com>
2023-08-18 15:56:22 -0300

    ago18

--- bao_pz/pdfs_cut.py
+++ bao_pz/pdfs_cut.py
@@ -30,3 +30,16 @@ def find_multi_modal(data,redshift_name,pdf_name,num_of_peaks):
 	peaks=[find_peaks(data[pdf_name][i])[0] for i in range(len(data))]
 	less_peaks=[i for i in range(len(peaks)) if len(peaks[i])==num_of_peaks]	
 	return less_peaks
+	
+def moments(pdf,x,n):
+	mu_n=pdf*(x)**n
+	return np.sum(mu_n)
+	
+def find_gaussians(data,x,output,n=2):
+	pdf=data['pdf']
+	mu2_s2=(data['Z_err']**2)+data['Z']**2
+	res=np.array([moments(pdf[i],np.linspace(0,2,100),2) for i in range(len(data))])
+
+	gaussian=np.where((.9<res/mu2_s2) & (res/mu2_s2<=1))[0]
+	Table(data[gaussian]).write(output,format='fits',overwrite=True)
+	return 
